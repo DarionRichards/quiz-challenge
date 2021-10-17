6a3eeb23e4f2dacac0585650d8a577446977e08a
Darion Richards <darionrichards@outlook.com>
2021-10-17 17:53:34 +0100

    Construct highscores

--- assets/javascript/highscores.js
+++ assets/javascript/highscores.js
@@ -1,9 +1,9 @@
-// get from ls
 // construct elements
 // add event listener to clear-btn
 // remove elements constructed
 
-const getFromLocalStorage = function(key, defaultValue) {
+const getFromLocalStorage = (key, defaultValue) => {
+
     const localStorageData = JSON.parse(localStorage.getItem(key));
 
     if (!localStorageData) {
@@ -13,11 +13,37 @@ const getFromLocalStorage = function(key, defaultValue) {
     }
 };
 
+const constructHighscores = (highscores) => {
+
+    const ulElement = document.createElement('ul');
+    ulElement.setAttribute('class', 'highscores-list');
+    ulElement.setAttribute('id', 'highscores-list');
+
+    for (let i = 0; i < highscores.length; i++) {
+
+        const highscore = highscores[i]
+
+        const liElement = document.createElement('li');
+        liElement.setAttribute('class', 'result-container');
+
+        const initials = document.createElement('div');
+        initials.innerText = highscore.initials
+
+        const score = document.createElement('div');
+        score.innerText = highscore.score
+
+        liElement.append(initials, score);
+        ulElement.append(liElement);
+    }
+
+    document.getElementById('highscore-container').append(ulElement);
+};
+
 const onLoad = () => {
 
-    // get highscores from LS
-    // render scores
+    const highscores = getFromLocalStorage('highscores', []);
 
+    constructHighscores(highscores);
 };
 
 
